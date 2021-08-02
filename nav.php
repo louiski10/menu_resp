fa83948b5530ddb03c1d971cbacf9d9d2ea440d0
louiski10 <82381363+louiski10@users.noreply.github.com>
2021-08-02 14:50:30 +0200

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=yes" />
+
+<link rel="stylesheet" href="nav.css">
+<!-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <label for="ch" id="lab"><i class="fa fa-bars fa-2x" aria-hidden="true"></i></label>
+        <input type="checkbox" id="ch"/>
+        <nav>
+            <a href="Accueil">Accueil</a>
+            <a href="Programmes">Programmes</a>
+            <a href="Service">Service</a>
+            <a href="S'inscrire">S'inscrire</a>
+            <a href="Contact">Contact</a>
+        </nav>
+    </header>
+    <main>
+        <p>
+
+        </p>
+    </main>
+</body>
+</html>
